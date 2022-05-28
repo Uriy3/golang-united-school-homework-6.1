43f27e8ad85f640f65e71c380438baa42b6c60b3
Uriy3 <99492882+Uriy3@users.noreply.github.com>
2022-05-28 23:46:07 +0300

    Update reactangle.go

--- reactangle.go
+++ reactangle.go
@@ -1,3 +1,4 @@
+
 package golang_united_school_homework
 
 // Rectangle must satisfy to Shape interface
@@ -5,3 +6,10 @@ type Rectangle struct {
 	Height, Weight float64
 }
 
+func (r *Rectangle) CalcPerimeter() float64 {
+	return r.Height*2 + r.Weight*2
+}
+
+func (r *Rectangle) CalcArea() float64 {
+	return r.Height * r.Weight
+}
